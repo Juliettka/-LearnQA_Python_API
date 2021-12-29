7316e4a9a23cf3ede1c04f55bffcfa961972ce33
JuliaSem <j.semenova@7Linternational.com>
2021-12-29 12:59:28 +0200

    Ex15: Тесты на метод user

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,9 +1,11 @@
+import pytest
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
 
 class TestUserRegister(BaseCase):
+
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
 
@@ -21,3 +23,77 @@ class TestUserRegister(BaseCase):
         Assertions.assert_status_code(response, 400)
         assert response.content.decode(
             "utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
+
+    def test_create_user_with_not_valid_email(self):
+        email = 'juliatestxample.com'
+        data = self.prepare_registration_data(email)
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_status_code(response, 400)
+        assert response.content.decode(
+            "utf-8") == f"Invalid email format", f"Unexpected response content {response.content}"
+
+    data = [
+            {
+                "username": "",
+                "firstName": "FirstName",
+                "lastName": "LastName",
+                "email": "test@example.com",
+                "password": "1234"
+            },
+            {
+                "username": "UserName",
+                "firstName": "",
+                "lastName": "LastName",
+                "email": "test@example.com",
+                "password": "1234"
+            },
+            {
+                "username": "UserName",
+                "firstName": "FirstName",
+                "lastName": "",
+                "email": "test@example.com",
+                "password": "1234"
+            },
+            {
+                "username": "UserName",
+                "firstName": "FirstName",
+                "lastName": "LastName",
+                "email": "",
+                "password": "1234"
+            },
+            {
+                "username": "UserName",
+                "firstName": "FirstName",
+                "lastName": "LastName",
+                "email": "test@test.com",
+                "password": ""
+            }
+        ]
+
+    @pytest.mark.parametrize('data', data)
+    def test_create_user_without_some_fields(self, data):
+        response2 = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_status_code(response2, 400)
+
+    def test_create_user_with_short_name(self):
+        data = self.prepare_registration_data()
+        data['firstName'] = "j"
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_status_code(response, 400)
+        assert response.content.decode(
+            "utf-8") == f"The value of 'firstName' field is too short", f"Unexpected response content {response.content}"
+
+    def test_create_user_with_long_name(self):
+        data = self.prepare_registration_data()
+        data['firstName'] = "j"*251
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_status_code(response, 400)
+        assert response.content.decode(
+            "utf-8") == f"The value of 'firstName' field is too long", f"Unexpected response content {response.content}"
